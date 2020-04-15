5f3af4c9a9efd9b9419f48afb27f667e0e10096f
Alexis-Dk <alexeydruzik@inbox.ru>
2020-04-15 18:18:44 +0300

    Refactoring.

--- CourseProjectDB/LoginForm.cs
+++ CourseProjectDB/LoginForm.cs
@@ -36,13 +36,16 @@ namespace CourseProjectDB
             DataTable dataTable = new DataTable();
             sqlDataAdapter.Fill(dataTable);
 
-            //dataGridView1.DataSource = dataTable;
-
             UserForm userForm = new UserForm(this);
-            userForm.Show();
-            this.Hide();
-
             
+            if(textBox1.Text == "alex" && textBox2.Text == "alex")
+            {
+                userForm.Show();
+                this.Hide();
+            } else
+            {
+                MessageBox.Show("Неправильный логин или пароль");
+            }
 
         }
 
