6774588dc792fa4e8ea56553a755d21d003c9b1c
Alexis-Dk <alexeydruzik@inbox.ru>
2020-04-16 14:37:04 +0300

    Added sql-file with functionals, procedures and so on.

---
+++ CourseProjectDB/db-fetures.sql
@@ -0,0 +1,10 @@
+﻿GO
+CREATE VIEW FullUserView([id], [last_name], [patronymic], [birthday], [login], [password], [money], [role_id], [status_id], [name]) AS
+SELECT u.id, u.id, u.id, u.id, u.id, u.id, u.id, u.id, u.id, s.name FROM 
+[carriages_system].[dbo].[user] AS u
+left join
+user_status AS s
+ON u.status_id = s.id
+
+SELECT * FROM FullUserView;
+DROP VIEW FullUserView
