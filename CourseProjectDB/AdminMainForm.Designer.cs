da617917214964e30da47be2bdd27ca18d177a6e
Alexis-Dk <alexeydruzik@inbox.ru>
2020-04-18 13:11:06 +0300

    Changed window size.

--- CourseProjectDB/AdminMainForm.Designer.cs
+++ CourseProjectDB/AdminMainForm.Designer.cs
@@ -360,7 +360,7 @@
             this.статусыПользователейToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1293, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1051, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -400,7 +400,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1293, 616);
+            this.panel1.Size = new System.Drawing.Size(1051, 616);
             this.panel1.TabIndex = 1;
             // 
             // brandBindingNavigator
@@ -584,7 +584,7 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1293, 616);
+            this.panel2.Size = new System.Drawing.Size(1051, 616);
             this.panel2.TabIndex = 2;
             // 
             // dataGridView1
@@ -778,7 +778,7 @@
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(0, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1293, 616);
+            this.panel3.Size = new System.Drawing.Size(1051, 616);
             this.panel3.TabIndex = 3;
             // 
             // dataGridView2
@@ -943,7 +943,7 @@
             this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel4.Location = new System.Drawing.Point(0, 0);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(1293, 616);
+            this.panel4.Size = new System.Drawing.Size(1051, 616);
             this.panel4.TabIndex = 4;
             // 
             // dataGridView3
@@ -1130,7 +1130,7 @@
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel5.Location = new System.Drawing.Point(0, 0);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(1293, 616);
+            this.panel5.Size = new System.Drawing.Size(1051, 616);
             this.panel5.TabIndex = 8;
             // 
             // dataGridView7
@@ -1295,7 +1295,7 @@
             this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel6.Location = new System.Drawing.Point(0, 0);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(1293, 616);
+            this.panel6.Size = new System.Drawing.Size(1051, 616);
             this.panel6.TabIndex = 7;
             // 
             // dataGridView6
@@ -1460,7 +1460,7 @@
             this.panel7.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel7.Location = new System.Drawing.Point(0, 0);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(1293, 616);
+            this.panel7.Size = new System.Drawing.Size(1051, 616);
             this.panel7.TabIndex = 6;
             // 
             // dataGridView5
@@ -1668,7 +1668,7 @@
             this.panel8.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel8.Location = new System.Drawing.Point(0, 0);
             this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(1293, 616);
+            this.panel8.Size = new System.Drawing.Size(1051, 616);
             this.panel8.TabIndex = 5;
             // 
             // dataGridView4
@@ -1841,7 +1841,7 @@
             this.panel9.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel9.Location = new System.Drawing.Point(0, 0);
             this.panel9.Name = "panel9";
-            this.panel9.Size = new System.Drawing.Size(1293, 616);
+            this.panel9.Size = new System.Drawing.Size(1051, 616);
             this.panel9.TabIndex = 8;
             // 
             // dataGridView9
@@ -2006,7 +2006,7 @@
             this.panel10.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel10.Location = new System.Drawing.Point(0, 0);
             this.panel10.Name = "panel10";
-            this.panel10.Size = new System.Drawing.Size(1293, 616);
+            this.panel10.Size = new System.Drawing.Size(1051, 616);
             this.panel10.TabIndex = 7;
             // 
             // dataGridView8
@@ -2286,7 +2286,7 @@
             this.panel11.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel11.Location = new System.Drawing.Point(0, 0);
             this.panel11.Name = "panel11";
-            this.panel11.Size = new System.Drawing.Size(1293, 616);
+            this.panel11.Size = new System.Drawing.Size(1051, 616);
             this.panel11.TabIndex = 11;
             // 
             // dataGridView10
@@ -2557,8 +2557,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1293, 616);
+            this.ClientSize = new System.Drawing.Size(1051, 616);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel11);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel6);
             this.Controls.Add(this.panel3);
@@ -2566,10 +2570,6 @@
             this.Controls.Add(this.panel8);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.panel11);
-            this.Controls.Add(this.panel9);
-            this.Controls.Add(this.panel10);
-            this.Controls.Add(this.panel5);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "AdminMainForm";
             this.Text = "Панель администратора";
